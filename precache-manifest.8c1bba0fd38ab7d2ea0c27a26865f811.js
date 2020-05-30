76a186fecba490d7f0d84bd034a0173920049e18
josvieira <josienevieira20@gmail.com>
2020-05-29 21:36:28 -0300

    Updates

---
+++ precache-manifest.8c1bba0fd38ab7d2ea0c27a26865f811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b21e4a3091ce4394da10e9b10e57b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3489b39531be769188da",
+    "url": "/static/css/main.395803d6.chunk.css"
+  },
+  {
+    "revision": "6323210bf314e141f97d",
+    "url": "/static/js/2.1ecc2831.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1ecc2831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3489b39531be769188da",
+    "url": "/static/js/main.6142d388.chunk.js"
+  },
+  {
+    "revision": "9e5a13853c4c27f372de",
+    "url": "/static/js/runtime-main.b94f7b04.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/static/media/logo.82a7c062.svg"
+  }
+]);
